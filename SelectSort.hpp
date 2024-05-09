2699e46c3a4201084c359816871e4ed44b62221f
wynhelloworld <wynhelloworld@gmail.com>
2024-05-09 20:01:32 +0800

    选择排序

---
+++ SelectSort.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+void SelectSort(std::vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
